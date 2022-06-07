608e2fdeee331db34297f61401a4166639e13321
maxnankivell <maxnankivell1@gmail.com>
2022-06-07 13:15:53 +1200

    start

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,36 @@
 
 <script setup lang="ts">
 import HelloWorld from "@/components/HelloWorld.vue";
+import { onMounted } from "vue";
+
+interface WordConnection {
+  [word: string]: string[];
+}
+
+const wordArray = ["tent", "bent", "rent", "fart", "farm", "done", "dart"];
+const objectWithWordConnections: WordConnection = {};
+
+onMounted(() => {
+  for (const outerWord of wordArray) {
+    for (const innerWord of wordArray) {
+      let sum = 0;
+      for (let i = 0; i < outerWord.length; i++) {
+        if (outerWord.charAt(i) == innerWord.charAt(i)) {
+          sum += 1;
+        }
+      }
+      if (sum === 3) {
+        if (objectWithWordConnections[outerWord]) {
+          objectWithWordConnections[outerWord].push(innerWord);
+        } else {
+          objectWithWordConnections[outerWord] = [innerWord];
+        }
+      }
+    }
+  }
+
+  console.log(objectWithWordConnections);
+});
 </script>
 
 <style scoped lang="scss"></style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "//",
   plugins: [vue()],
   resolve: {
     alias: {
