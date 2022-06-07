fbe887fc76440c7ab0b5c241da093d8a0b1b03f9
maxnankivell <maxnankivell1@gmail.com>
2022-06-07 12:44:26 +1200

    setup

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,69 @@
+<template>
+  <h1>{{ finalMessage }}</h1>
+
+  <p>
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+  </p>
+
+  <p>See <code>README.md</code> for more information.</p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Docs
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+  </p>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script setup lang="ts">
+import { defineProps, ref, withDefaults } from "vue";
+
+interface Props {
+  msg: string;
+  test?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  test: "hello",
+});
+
+const finalMessage = ref(props.msg + props.test);
+
+const count = ref(0);
+</script>
+
+<style scoped lang="scss">
+$its-a-color: #304455;
+
+h1 {
+  background-color: $variable;
+}
+
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+p {
+  code {
+    background-color: #eee;
+    padding: 2px 4px;
+    border-radius: 4px;
+    color: $its-a-color;
+  }
+}
+</style>
